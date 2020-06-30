f65f13c3432b3b87ea2745d85e65f842e50cd378
nobodyxu <Jiahao_XU@outlook.com>
2020-06-30 18:47:26 +1000

    Use assert for programming error in Url::set_*

--- curl_url.cc
+++ curl_url.cc
@@ -1,5 +1,6 @@
 #include "curl.hpp"
 #include <curl/curl.h>
+#include <cassert>
 
 namespace curl {
 Url::Url():
@@ -41,21 +42,28 @@ Url& Url::operator = (Url &&other) noexcept
     return *this;
 }
 
+static auto curl_urlset_wrapper(void *url, CURLUPart part, const char *arg)
+{
+    auto code = curl_url_set(static_cast<CURLU*>(url), part, arg, 0);
+    assert(code != CURLUE_BAD_HANDLE);
+    assert(code !=  CURLUE_BAD_PARTPOINTER);
+    return code;
+}
 void Url::set_url(const char *url_arg)
 {
-    check_url(curl_url_set(static_cast<CURLU*>(url), CURLUPART_URL, url_arg, 0));
+    check_url(curl_urlset_wrapper(url, CURLUPART_URL, url_arg));
 }
 void Url::set_scheme(const char *scheme)
 {
-    check_url(curl_url_set(static_cast<CURLU*>(url), CURLUPART_SCHEME, scheme, 0));
+    check_url(curl_urlset_wrapper(url, CURLUPART_SCHEME, scheme));
 }
 void Url::set_options(const char *options)
 {
-    check_url(curl_url_set(static_cast<CURLU*>(url), CURLUPART_OPTIONS, options, 0));
+    check_url(curl_urlset_wrapper(url, CURLUPART_OPTIONS, options));
 }
 void Url::set_query(const char *query)
 {
-    check_url(curl_url_set(static_cast<CURLU*>(url), CURLUPART_QUERY, query, 0));
+    check_url(curl_urlset_wrapper(url, CURLUPART_QUERY, query));
 }
 
 auto Url::get_url() const -> fullurl_str
