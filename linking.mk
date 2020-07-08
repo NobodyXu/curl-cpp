b8be1e033ee3c424098e58b490b3fe88cbd0f052
nobodyxu <Jiahao_XU@outlook.com>
2020-07-08 20:54:32 +1000

    Remove CXXFLAGS that can cause binary incompatible

--- linking.mk
+++ linking.mk
@@ -1,2 +1,2 @@
-CXXFLAGS := -std=c++17 -freg-struct-return -fshort-enums -flto -fno-fat-lto-objects -O2 $(shell curl-config --cflags)
+CXXFLAGS := -std=c++17 -flto -fno-fat-lto-objects -O2 $(shell curl-config --cflags)
 LDFLAGS += $(shell curl-config --libs)
