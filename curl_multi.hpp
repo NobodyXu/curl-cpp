55bea5620620cbc7882010c3169d389595c73068
nobodyxu <Jiahao_XU@outlook.com>
2020-07-10 16:27:05 +1000

    Add doc on default setting for Multi_t::set_multiplexing

--- curl_multi.hpp
+++ curl_multi.hpp
@@ -72,6 +72,8 @@ public:
      *                              Should be between [1, 2 ^ 31 - 1].
      *                              Set it to 1 or 0 disable multiplexing.
      *
+     * Since curl_t::version >= 7.62.0, multiplex is turned on by default.
+     *
      * NOTE that libcurl not always accept max_concurrent_stream tuning.
      * Check curl_t::has_max_concurrent_stream_support().
      *
