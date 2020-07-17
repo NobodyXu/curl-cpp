c29e73b0976869cb7f9d7b31c0bb0d8156a300aa
nobodyxu <Jiahao_XU@outlook.com>
2020-07-17 15:45:30 +1000

    Use abs path for bind mount in test/web_server
    docker only accepts absoulte path

--- test/web_server/run_nginx.sh
+++ test/web_server/run_nginx.sh
@@ -8,7 +8,9 @@ if ! container_up $container_name; then
         $DOCKER start $container_name
     else
         echo Creating $container_name
-        $DOCKER run --name $container_name -p 8787:80 -d --mount=type=bind,src=$(dirname $0),dst=/usr/share/nginx/html,ro=true nginx:stable
+        $DOCKER run --name $container_name -p 8787:80 -d \
+                    --mount=type=bind,src=$(pwd)/$(dirname $0),dst=/usr/share/nginx/html,ro=true \
+                    nginx:stable
     fi
 else
     echo $container_name is already running!
