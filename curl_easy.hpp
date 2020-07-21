a678f4f77e7e18f44b8d20b2b22f0a16c448ea98
nobodyxu <Jiahao_XU@outlook.com>
2020-07-21 13:09:19 +1000

    Improve doc on retval of getinfo_transfer_time

--- curl_easy.hpp
+++ curl_easy.hpp
@@ -640,6 +640,7 @@ public:
      * @pre curl_t::has_redirect_url_support() && 
      *      url is set to use http(s) && curl_t::has_protocol("http")
      * @return null-terminated string, freeing not required.
+     *         <br>Would be freed up when corresponding curl::Easy_t is destroyed.
      *
      * If you disable redirection or CURLOPT_MAXREDIRS limit 
      * prevented a redirect to happen (since 7.54.1), 
