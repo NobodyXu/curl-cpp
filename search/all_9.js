1be0d5e6c1ba2e207c26d5408f23d47455172424
nobodyxu <Jiahao_XU@outlook.com>
2020-07-22 18:42:51 +1000

    Update github pages

--- search/all_9.js
+++ search/all_9.js
@@ -3,5 +3,5 @@ var searchData=
   ['malform_5finput_49',['malform_input',['../classcurl_1_1Url__ref__t.html#a21ac5e8f74e0ea461b9d050f431fbd55a48acc9d6bdaca34e0358116dd19eb5d4',1,'curl::Url_ref_t']]],
   ['multi_5fassign_50',['multi_assign',['../classcurl_1_1Multi__t.html#ae7fc43c375eb97ee584bc8592cd16fc6',1,'curl::Multi_t']]],
   ['multi_5fsocket_5faction_51',['multi_socket_action',['../classcurl_1_1Multi__t.html#a3ec91024af38c4a687811bb6444c9652',1,'curl::Multi_t']]],
-  ['multi_5ft_52',['Multi_t',['../classcurl_1_1Multi__t.html',1,'curl::Multi_t'],['../classcurl_1_1Multi__t.html#a0fa5766946a2e509fb96d91f1c46a783',1,'curl::Multi_t::Multi_t()']]]
+  ['multi_5ft_52',['Multi_t',['../classcurl_1_1Multi__t.html',1,'curl::Multi_t'],['../classcurl_1_1Multi__t.html#ae096fc37dd8a2d1244bfa96d7d18d3e0',1,'curl::Multi_t::Multi_t()=default'],['../classcurl_1_1Multi__t.html#aa03b009bc391b6e8e4a980fed48b1c74',1,'curl::Multi_t::Multi_t(void *multi) noexcept'],['../classcurl_1_1Multi__t.html#a0fa5766946a2e509fb96d91f1c46a783',1,'curl::Multi_t::Multi_t(Multi_t &amp;&amp;) noexcept']]]
 ];
